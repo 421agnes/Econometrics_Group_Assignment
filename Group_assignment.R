8af0e18607f60054ef148bc89a4759336cd35ec2
421agnes <421agnes@gmail.com>
2025-10-27 22:51:13 +0100

    version 16

--- Group_assignment.R
+++ Group_assignment.R
@@ -359,27 +359,27 @@ model_diesel_11 <- lm(log(avg_diesel) ~ km_quick + only_from_motorway + motorway
 summary(model_diesel_11)
 
 #linear regression for gasoline
-model_gasoline_12 <- lm(log(avg_petrol) ~ min_quick +only_from_motorway + motorway + Resident + big_companies ,data = data)
+model_gasoline_12 <- lm(log(avg_petrol) ~ min_quick + motorway + Resident + big_companies ,data = data)
 summary(model_gasoline_12)
 
 #linear regression for diesel
-model_diesel_12 <- lm(log(avg_diesel) ~ min_quick + only_from_motorway + motorway + Resident + big_companies ,data = data)
+model_diesel_12 <- lm(log(avg_diesel) ~ min_quick + motorway + Resident + big_companies ,data = data)
 summary(model_diesel_12)
 
 #linear regression for gasoline
-model_gasoline_13 <- lm(log(avg_petrol) ~ min_quick + only_from_motorway + motorway + log(Earnings) + Resident + big_companies ,data = data)
+model_gasoline_13 <- lm(log(avg_petrol) ~ min_quick + motorway + log(Earnings) + Resident + big_companies ,data = data)
 summary(model_gasoline_13)
 
 #linear regression for diesel
-model_diesel_13 <- lm(log(avg_diesel) ~ min_quick + only_from_motorway + motorway + log(Earnings) + Resident + big_companies ,data = data)
+model_diesel_13 <- lm(log(avg_diesel) ~ min_quick + motorway + log(Earnings) + Resident + big_companies ,data = data)
 summary(model_diesel_13)
 
 #linear regression for gasoline
-model_gasoline_14 <- lm(log(avg_petrol) ~ min_quick + only_from_motorway + motorway + Status_capital + Status_city + Status_village + Status_maincities + Resident + big_companies ,data = data)
+model_gasoline_14 <- lm(log(avg_petrol) ~ min_quick + motorway + Status_capital + Status_city + Status_village + Status_maincities + Resident + big_companies ,data = data)
 summary(model_gasoline_14)
 
 #linear regression for diesel
-model_diesel_14 <- lm(log(avg_diesel) ~ min_quick + only_from_motorway + motorway +  Status_capital + Status_city + Status_village + Status_maincities + Resident + big_companies ,data = data)
+model_diesel_14 <- lm(log(avg_diesel) ~ min_quick + motorway +  Status_capital + Status_city + Status_village + Status_maincities + Resident + big_companies ,data = data)
 summary(model_diesel_14)
 
 # Create the stargazer diesel
